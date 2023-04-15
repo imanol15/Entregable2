fb3d65b62bbc5d686cb8a15d2176c3467633114a
Imanol <Imanol.anda@opendeusto.es>
2023-04-15 13:35:16 +0200

    cambios urls

---
+++ gestionPedidos/Pedidos/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('productos/', views.lista_productos, name='lista_productos'),
+    path('productos/<int:referencia>/', views.detalle_producto, name='detalle_producto'),
+    path('productos/crear/', views.crear_producto, name='crear_producto'),
+    path('productos/<int:referencia>/editar/', views.editar_producto, name='editar_producto'),
+    path('pedidos/', views.lista_pedidos, name='lista_pedidos'),
+    path('pedidos/<int:Pedido_codigo_referencia>/', views.detalle_pedido, name='detalle_pedido'),
+    path('pedidos/crear/', views.crear_pedido, name='crear_pedido'),
+]
--- gestionPedidos/gestionPedidos/urls.py
+++ gestionPedidos/gestionPedidos/urls.py
@@ -1,13 +1,9 @@
 
-from django.urls import path
-from . import views
+
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('productos/', views.lista_productos, name='lista_productos'),
-    path('productos/<int:referencia>/', views.detalle_producto, name='detalle_producto'),
-    path('productos/crear/', views.crear_producto, name='crear_producto'),
-    path('productos/<int:referencia>/editar/', views.editar_producto, name='editar_producto'),
-    path('pedidos/', views.lista_pedidos, name='lista_pedidos'),
-    path('pedidos/<int:Pedido_codigo_referencia>/', views.detalle_pedido, name='detalle_pedido'),
-    path('pedidos/crear/', views.crear_pedido, name='crear_pedido'),
+ path('appEmpresaDjango/', include('appEmpresaDjango.urls')),
+ path('admin/', admin.site.urls),
 ]
