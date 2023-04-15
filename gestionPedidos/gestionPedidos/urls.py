a7ce384b4d2a0d23532f1df82678ee05d293d8e2
Imanol <Imanol.anda@opendeusto.es>
2023-04-15 13:37:06 +0200

    cambio urls

--- gestionPedidos/gestionPedidos/urls.py
+++ gestionPedidos/gestionPedidos/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
- path('appEmpresaDjango/', include('appEmpresaDjango.urls')),
+ path('pedidos/', include('pedidos.urls')),
  path('admin/', admin.site.urls),
 ]
