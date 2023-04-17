b3f8f6333f44e460d801e6053f47f4e6e6baa8fe
Imanol <Imanol.anda@opendeusto.es>
2023-04-17 16:19:22 +0200

    Añadido de Cliente

--- gestionPedidos/Pedidos/admin.py
+++ gestionPedidos/Pedidos/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Componente,Producto,Pedido
+from .models import Componente,Producto,Pedido,Cliente
 admin.site.register(Componente)
 admin.site.register(Producto)
 admin.site.register(Pedido)
+admin.site.register(Cliente)
--- gestionPedidos/Pedidos/models.py
+++ gestionPedidos/Pedidos/models.py
@@ -12,16 +12,23 @@ class Producto(models.Model):
     descripcion = models.TextField()
     categoria = models.CharField(max_length=50)
     componentes = models.ForeignKey(Componente, on_delete=models.CASCADE)
-
-class Pedido(models.Model):
-    Pedido_codigo_referencia = models.CharField(max_length=50, unique=True)
-    fecha = models.DateField()
+    
+class Cliente(models.Model):
     cif_cliente = models.CharField(max_length=50)
     nombre_empresa_cliente = models.CharField(max_length=100)
     direccion_cliente = models.CharField(max_length=200)
     datos_contacto_cliente = models.CharField(max_length=200)
+    
+
+class Pedido(models.Model):
+    Pedido_codigo_referencia = models.CharField(max_length=50, unique=True)
+    fecha = models.DateField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     producto_solicitado = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidad = models.IntegerField()
     precio_total = models.DecimalField(max_digits=10, decimal_places=2)
 
+    
+
+          
     
