04bb6a66c0b50ec92ec8f19308c5a47330dbf90a
Imanol <Imanol.anda@opendeusto.es>
2023-04-18 18:01:22 +0200

    cambios models y admin

--- gestionPedidos/Pedidos/admin.py
+++ gestionPedidos/Pedidos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Componente,Producto,Pedido,Cliente
+from .models import Componente,Producto,Pedido,Cliente,Producto_pedido
 admin.site.register(Componente)
 admin.site.register(Producto)
 admin.site.register(Pedido)
 admin.site.register(Cliente)
+admin.site.register(Producto_pedido)
--- gestionPedidos/Pedidos/models.py
+++ gestionPedidos/Pedidos/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-
+# ENtre las clases Componente y Producto hemos elegido que sea 1-n, es decir, un componente tiene varios Productos 
+# y varios productos pueden tener el mismo componente, aunque tambien podria ser una n-m
+#  que varios componentes sean de varios productos y viceversa
 class Componente(models.Model):
-    Componentes_codigo_referencia = models.CharField(max_length=50, unique=True)
+    componentes_codigo_referencia = models.CharField(max_length=50, unique=True)
     nombre_modelo = models.CharField(max_length=100)
     marca = models.CharField(max_length=70)
+
 
 class Producto(models.Model):
     referencia = models.CharField(max_length=50, unique=True)
@@ -21,12 +24,17 @@ class Cliente(models.Model):
     
 
 class Pedido(models.Model):
-    Pedido_codigo_referencia = models.CharField(max_length=50, unique=True)
+    pedido_codigo_referencia = models.CharField(max_length=50, unique=True)
     fecha = models.DateField()
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    producto_solicitado = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    cantidad = models.IntegerField()
     precio_total = models.DecimalField(max_digits=10, decimal_places=2)
+# Hemos hecho una conexion n-m para producto pedido, es decir, varios pedidos pueden ser de varios productos y viceersa
+class Producto_pedido(models.Model):
+    producto_solicitado = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    producto_solicitado = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    
+
 
     
 
