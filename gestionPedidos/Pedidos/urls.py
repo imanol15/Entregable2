a34dab0e826c3b723ae86e8d8ec0adc26e4c40bd
Imanol <Imanol.anda@opendeusto.es>
2023-04-19 15:53:13 +0200

    cambios para poder runear

--- gestionPedidos/Pedidos/urls.py
+++ gestionPedidos/Pedidos/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('productos/', views.lista_productos, name='lista_productos'),
-    path('productos/<int:referencia>/', views.detalle_producto, name='detalle_producto'),
-    path('pedidos/', views.lista_pedidos, name='lista_pedidos'),
-    path('pedidos/<int:Pedido_codigo_referencia>/', views.detalle_pedido, name='detalle_pedido'),
+    
 ]
--- gestionPedidos/gestionPedidos/urls.py
+++ gestionPedidos/gestionPedidos/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
- path('pedidos/', include('pedidos.urls')),
+ path('Pedidos/', include('Pedidos.urls')),
  path('admin/', admin.site.urls),
 ]
