b39b05714ad63ab4ff8c6f35ea708eadb007cc54
Hezitzaile <iban.argote@opendesuto.es>
2023-04-22 13:14:50 +0200

    url

--- gestionPedidos/Pedidos/urls.py
+++ gestionPedidos/Pedidos/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from .views import ComponentListView, ComponentCreateView, ComponentUpdateView, ComponentDeleteView, ComponentDetailView
 
 urlpatterns = [
+
+    path('', ComponentListView.as_view(), name='component-list'),
+    path('new/', ComponentCreateView.as_view(), name='component-create'),
+    path('<int:pk>/', ComponentDetailView.as_view(), name='component-detail'),
+    path('<int:pk>/update/', ComponentUpdateView.as_view(), name='component-update'),
+    path('<int:pk>/delete/', ComponentDeleteView.as_view(), name='component-delete'),
     
 ]
