9f8b4c76e2aa8b81346c63898e1e5ede024ddae6
Imanol <Imanol.anda@opendeusto.es>
2023-04-24 23:40:13 +0200

    cambio para arreglar el css

--- gestionPedidos/gestionPedidos/settings.py
+++ gestionPedidos/gestionPedidos/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 
-
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'gestionPedidos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,11 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'Pedidos\static'),
+    BASE_DIR / "static",
+    
 ]
-
 
 
 # Default primary key field type
