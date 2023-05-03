3c876b7debcf0f60bcf0fd2634c4b40acc689a9a
Imanol <Imanol.anda@opendeusto.es>
2023-05-03 15:02:50 +0200

    commit para la entrega

--- gestionPedidos/Pedidos/admin.py
+++ gestionPedidos/Pedidos/admin.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
 from .models import Componente,Producto,Pedido,Cliente,ProductoPedido
 
-class ProductoPedidoInline(admin.TabularInline):
-    model = ProductoPedido
-    extra = 1
 
-class PedidoAdmin(admin.ModelAdmin):
-    inlines = [ProductoPedidoInline]
-    list_display = ('codigo_referencia', 'fecha', 'cliente', 'preciototal', 'obtener_cantidades')
-
+admin.site.register(ProductoPedido)
 admin.site.register(Componente)
 admin.site.register(Producto)
-admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(Pedido)
 admin.site.register(Cliente)
 
 
