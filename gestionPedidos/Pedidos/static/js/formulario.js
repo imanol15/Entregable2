7ac3af1409fb555c7209b80c0f9745e645cb4058
Imanol <Imanol.anda@opendeusto.es>
2023-05-20 09:52:23 +0200

    cambios

---
+++ gestionPedidos/Pedidos/static/js/formulario.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var productoSolicitadoField = document.getElementById('id_producto_solicitado');
+    var pedidoSolicitadoField = document.getElementById('pedidoSolicitadoField');
+    var cantidadField = document.getElementById('cantidadField');
+
+    productoSolicitadoField.addEventListener('change', function() {
+      if (productoSolicitadoField.value !== '') {
+        pedidoSolicitadoField.style.display = 'block';
+      } else {
+        pedidoSolicitadoField.style.display = 'none';
+        cantidadField.style.display = 'none';
+      }
+    });
+
+    pedidoSolicitadoField.addEventListener('change', function() {
+      if (pedidoSolicitadoField.value !== '') {
+        cantidadField.style.display = 'block';
+      } else {
+        cantidadField.style.display = 'none';
+      }
+    });
+  });
